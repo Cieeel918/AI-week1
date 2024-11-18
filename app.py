37022b255b8de4cc2f3edd88eff8445f2ca95ff0
Cieeel918 <1592483563@qq.com>
2024-11-18 12:22:05 +0000

    main

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)  # 创建一个 Flask 应用实例
 def index():
     return(render_template('index.html'))  # 定义 index 函数，当访问根路径时，渲染并返回 index.html 模板。
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    name=request.form.get("q")
+    return(render_template('main.html'))
+
 if __name__=='__main__':
     app.run()
 
